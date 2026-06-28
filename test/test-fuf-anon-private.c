584b109c192d70f60dd42b496e1862b6e680eb88
StefanoBelli <stefano9913@gmail.com>
2026-06-28 11:53:55 +0200

    added fuf tests

--- test/test-fuf-anon-private.c
+++ test/test-fuf-anon-private.c
@@ -14,7 +14,7 @@ int main()
 	enable_testing_for_me(SUBSYS_NAME);
 	start_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
 
-	/* do nothing - cpr must not be called */
+	/* PTE is non-mapped - DO NOT expect any freeing */
 	{
 		/* PREPARING: do the mmap */
 		char *mem = (char*) mmap(
@@ -24,19 +24,165 @@ int main()
 				-1, 0);
 		die_if(mem == MAP_FAILED);
 
-		spurious_byte_memwrite(mem, 'a');
+		munmap(mem, 10 * PAGE_SIZE);
 
 		{
 			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
 
 			test_int_eq_hard(entry, 0);
 		}
-
-		munmap(mem, 10 * PAGE_SIZE);
 	}
 
 	RESET_ALL();
 
+	/* PTE gets populated with zeropage - DO NOT expect any freeing*/
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		spurious_byte_memread(ch, page_nr(1));
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_eq_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
+
+	/* PTE is non-mapped -- syscall read and fixup code! - DO NOT expect any freeing */
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		die_if(trigger_syscall_pageread(page_nr(1), 10));
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_eq_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
+
+	/* my own page gets mapped - syscall write - 
+	 * EXPECT the free either from current thread's 
+	 * kernel control path or another thread */
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		die_if(trigger_syscall_pagewrite(page_nr(1), 10));
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_ge_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
+
+	/* my own page gets mapped - store instruction -
+	 * EXPECT the free as explained above */
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		spurious_byte_memwrite(page_nr(1), 'a');
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_ge_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
+
+	/* multiple pages get written - syscall write - 
+	 * EXPECT the free */
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		die_if(trigger_syscall_pagewrite(page_nr(1), 10));
+		die_if(trigger_syscall_pagewrite(page_nr(2), 10));
+		die_if(trigger_syscall_pagewrite(page_nr(5), 30));
+		die_if(trigger_syscall_pagewrite(page_nr(7), 40));
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_ge_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
+
+	/* multiple pages are written - store instruction -
+	 * EXPECT the free as explained above */
+	{
+		/* PREPARING: do the mmap */
+		char *mem = (char*) mmap(
+				NULL, 10 * PAGE_SIZE, 
+				PROT_READ | PROT_WRITE, 
+				MAP_PRIVATE | MAP_ANONYMOUS, 
+				-1, 0);
+		die_if(mem == MAP_FAILED);
+
+		spurious_byte_memwrite(page_nr(1), 'a');
+		spurious_byte_memwrite(page_nr(2), 'a');
+		spurious_byte_memwrite(page_nr(5), 'a');
+		spurious_byte_memwrite(page_nr(7), 'a');
+
+		munmap(mem, 10 * PAGE_SIZE);
+
+		{
+			int entry = query_int_value_testing_for_me(SUBSYS_NAME, ENTRY_KEY);
+
+			test_int_ge_hard(entry, 0);
+		}
+	}
+
+	RESET_ALL();
 	test_passed();
 
 __finish:
