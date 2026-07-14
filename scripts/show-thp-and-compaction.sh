067b0f89194158df047bc85ca539ac4e7f51906a
StefanoBelli <stefano9913@gmail.com>
2026-07-14 11:08:48 +0200

    Update script

--- scripts/show-thp-and-compaction.sh
+++ scripts/show-thp-and-compaction.sh
@@ -1,23 +1,17 @@
 #/bin/bash
 
-SYSFS_PSEUDO_FILES="
+PSEUDO_FILES="
 	/sys/kernel/mm/transparent_hugepage/enabled
 	/sys/kernel/mm/transparent_hugepage/defrag
 	/sys/kernel/mm/transparent_hugepage/shmem_enabled
 	/sys/kernel/mm/transparent_hugepage/hugepages-*kB/enabled
 	/sys/kernel/mm/transparent_hugepage/hugepages-*kB/shmem_enabled
-"
-
-PROCFS_COMPACTION="
 	/proc/sys/vm/compaction_proactiveness
 "
 
-for file in $SYSFS_PSEUDO_FILES; do
+for file in $PSEUDO_FILES; do
 	echo " *** $file *** "
 	cat $file
 	echo
 	echo
 done
-
-echo " *** $PROCFS_COMPACTION *** "
-cat $PROCFS_COMPACTION
