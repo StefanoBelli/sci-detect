d865d3cfa6dbb948717092a1fa461d7552828b9c
StefanoBelli <stefano9913@gmail.com>
2026-07-17 13:32:45 +0200

    More stuff, need to fix unpatched return thunk (x86 mitigations), use after free bug and kcps kmem cache issues

--- src/kernel/hooks/pte-page-track/hpf.c
+++ src/kernel/hooks/pte-page-track/hpf.c
@@ -5,7 +5,13 @@
 
 #include <vmfs.h>
 #include <logging.h>
-#include <kpsleepable.h>
+#include <ptealtprot.h>
+
+#ifdef DO_PTE_ALT_PROT
+#	include <linux/rcupdate.h>
+#	include <pgtrack.h>
+#	include <kpsleepable.h>
+#endif
 
 #define handle_pte_fault__symbol "handle_pte_fault"
 
@@ -34,11 +40,65 @@ static int handle_pte_fault__ehkrphook(
 	return 0;
 }
 
+struct kretprobe handle_pte_fault__krp;
+
 static int handle_pte_fault__hkrphook(
 		struct kretprobe_instance *krpi, __maybe_unused struct pt_regs *regs)
 {
-	del_vmf(*((struct vm_fault_entry**)krpi->data));
+	struct vm_fault_entry *vmfe = *((struct vm_fault_entry**) krpi->data);
 
+#ifdef DO_PTE_ALT_PROT
+	struct page_status *pgs;
+	pte_t pte;
+	unsigned long pfn;
+	bool pfn_found;
+	pte_t *vmf_ptep = vmfe->vmf->pte;
+	enum fault_flag vmf_flags = vmfe->vmf->flags;
+
+	if(!vmf_ptep)
+		return 0;
+
+	/* 
+	 * try not to acquire the ptl since this is 
+	 * only an atomic read, let's see... 
+	 */
+	pte = ptep_get(vmf_ptep);
+	pfn = page_to_pfn(pte_page(pte));
+
+	rcu_read_lock();
+	pgs = lookup_pfn_pgtrack(pfn);
+	pfn_found = pgs && try_page_status_get(pgs);
+	rcu_read_unlock();
+
+	if(likely(pfn_found)) {
+		if(likely(!pgs->pap))
+			goto __put_pgs_end;
+
+		struct kprobe *this_kp = kpat(handle_pte_fault__krp, krpi);
+
+		__enable_sleep(this_kp);
+
+		/* 
+		 * there is no risk of deadlock: skip_lock_this_mm is NULL.
+		 * In the worst case, the page fault handler still has
+		 * the mmap_read_lock taken (or the per-VMA lock?) but
+		 * still, are acquiring the semaphore for reading
+		 */
+		mutex_lock(&pgs->pap->lock);
+		wrex_locked_ptealtprot(pgs, vmf_flags, NULL);
+		mutex_unlock(&pgs->pap->lock);
+
+		__disable_sleep(this_kp);
+	} else 
+		goto __end;
+
+__put_pgs_end:
+	page_status_put(pgs);
+__end:
+
+#endif /* DO_PTE_ALT_PROT */
+
+	del_vmf(vmfe);
 	return 0;
 }
 
